90771b14a9668e16b90a455fe4333ed0ec184b42
manchalsharma08 <159640059+manchalsharma08@users.noreply.github.com>
2026-04-07 22:05:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,21 +1,28 @@
+
+----------------------------------------
 terraform {
+  required_version = ">= 1.5.0"
+
   backend "azurerm" {
-  resource_group_name = "MyRGc"
-  storage_account_name = "myrgst5679807578jhj"
-  container_name = "myrgccon"
-  key                  = "dev.terraform.tfstate"
-use_azuread_auth     = true    
+    resource_group_name  = "MyRGc"
+    storage_account_name = "myrgst5679807578jhj"
+    container_name       = "myrgccon"
+    key                  = "dev.terraform.tfstate"
+    use_azuread_auth     = true
   }
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.57.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "azurerm" {
-features {}
-subscription_id = "89ce96dc-8535-4140-a60a-4ddca5655e3a"
-  
+  features {}
+  subscription_id = "89ce96dc-8535-4140-a60a-4ddca5655e3a"
+  tenant_id            = "487b2b49-d378-4a01-8f7e-b709391bd996"
 }
+
+
